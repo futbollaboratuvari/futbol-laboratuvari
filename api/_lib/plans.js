0ba9d95b2251651cdf739586d33c24c39126ff2e
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-06-19 21:39:44 +0300

    API paket kullanim surelerini ekle

--- api/_lib/plans.js
+++ api/_lib/plans.js
@@ -4,18 +4,27 @@ const PLANS = {
     name: "Başlangıç",
     amountKurus: 14900,
     durationDays: 30,
+    monthlyUses: 10,
+    dailyUses: 3,
+    renewalType: "monthly",
   },
   pro: {
     id: "pro",
     name: "Pro Analiz",
     amountKurus: 29900,
     durationDays: 30,
+    monthlyUses: 40,
+    dailyUses: 10,
+    renewalType: "monthly",
   },
   vip: {
     id: "vip",
     name: "VIP Kupon",
     amountKurus: 49900,
     durationDays: 30,
+    monthlyUses: 120,
+    dailyUses: 30,
+    renewalType: "monthly",
   },
 };
 
