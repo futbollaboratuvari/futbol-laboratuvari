43bcdfe0641ee4159d80704f0deed48968e2be21
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-06-20 20:48:57 +0300

    Cache version dosyası 2130 sürümüne eşitlendi

--- cache-version.js
+++ cache-version.js
@@ -1,4 +1,4 @@
 (() => {
-  document.documentElement.setAttribute("data-fl-cache-version", "20260620-2125");
-  window.FUTBOL_LAB_CACHE_VERSION = "20260620-2125";
+  document.documentElement.setAttribute("data-fl-cache-version", "20260620-2130");
+  window.FUTBOL_LAB_CACHE_VERSION = "20260620-2130";
 })();
