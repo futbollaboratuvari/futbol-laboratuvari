90e3fc3134fe929161d43be1cdcee8fc8991b7de
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-06-20 18:42:41 +0300

    Ana sayfa boş alan ve bölüm taşıma hatası düzeltildi

--- nav-routing.js
+++ nav-routing.js
@@ -1,7 +1,7 @@
 // Site içi tek sayfa yönlendirmeleri
 // Tek yetkili router: menü, hero butonları ve dinamik paneller burada yönetilir.
 (() => {
-  const VERSION = "20260620-1845";
+  const VERSION = "20260620-1900";
   let syntheticLoadTimer = null;
   let observerReady = false;
 
