70198ea4c222d0a3e156cc486dd1663a72d08e6c
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-06-20 20:43:07 +0300

    Cache version dosyası 2125 sürümüne eşitlendi

--- cache-version.js
+++ cache-version.js
@@ -1,4 +1,4 @@
 (() => {
-  document.documentElement.setAttribute("data-fl-cache-version", "20260620-2120");
-  window.FUTBOL_LAB_CACHE_VERSION = "20260620-2120";
+  document.documentElement.setAttribute("data-fl-cache-version", "20260620-2125");
+  window.FUTBOL_LAB_CACHE_VERSION = "20260620-2125";
 })();
