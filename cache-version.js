b6060a864334dc9e52e9adb1c70ec5d031d69ef9
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-06-23 19:56:28 +0300

    Eski üye erişimi bir defalık sıfırlandı

--- cache-version.js
+++ cache-version.js
@@ -1,4 +1,14 @@
 (() => {
-  const version = "20260622-pages-v7";
+  const version = "20260622-pages-v8";
+  const resetKey = "fl_access_reset_20260622_v8";
+
+  if (localStorage.getItem(resetKey) !== "1") {
+    localStorage.removeItem("fl_premium_beta_access");
+    localStorage.removeItem("fl_premium_access_note");
+    localStorage.removeItem("fl_premium_access_level");
+    localStorage.removeItem("fl_premium_code_entered");
+    localStorage.setItem(resetKey, "1");
+  }
+
   document.documentElement.dataset.flCacheVersion = version;
 })();
