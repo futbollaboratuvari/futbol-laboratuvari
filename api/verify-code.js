5252561fcd122274d29741d1d7daa218ade34fd0
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-08-22 01:50:04 +0300

    Use shared backend outside api

--- api/verify-code.js
+++ api/verify-code.js
@@ -1,6 +1,5 @@
 const crypto = require("crypto");
-const { appendUsageRecord } = require("./lib/usage-write");
-const { decreaseMembershipCount } = require("./lib/membership-decrement");
+const { appendUsageRecord, decreaseMembershipCount } = require("../server-lib/backend");
 
 const CODE_DATABASE = {
   "d0e366399638702f7f4fd5cae64e544617bc4ec948a277a34c2a9d7cb855d290": {
