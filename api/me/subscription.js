499aa57e2fee48fa31941a2c8424772bdb236abb
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-08-22 12:26:34 +0300

    Use shared helpers outside Vercel API directory

--- api/me/subscription.js
+++ api/me/subscription.js
@@ -1,4 +1,4 @@
-const { json } = require("../_lib/http");
+const { json } = require("../../server-lib/_lib/http");
 
 module.exports = async function handler(req, res) {
   if (req.method !== "GET") {
