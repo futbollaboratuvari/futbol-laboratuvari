68201bfe760245deac27b89d73ed6251e902863e
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-09-18 22:26:53 +0300

    Bump live realtime cache version

--- cache-version.js
+++ cache-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "20260918-live-analysis-robot-v1";
+  const version = "20260918-live-realtime-v1";
   const resetKey = "fl_membership_full_reset_20260622_v9";
 
   if (localStorage.getItem(resetKey) !== "1") {
