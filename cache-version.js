a75601bfe69332afaac208b82389e6b2828dcbd5
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-09-18 15:27:07 +0300

    Bump live power cache version

--- cache-version.js
+++ cache-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "20260918-live-power-stable-v1";
+  const version = "20260918-live-power-stable-v2";
   const resetKey = "fl_membership_full_reset_20260622_v9";
 
   if (localStorage.getItem(resetKey) !== "1") {
