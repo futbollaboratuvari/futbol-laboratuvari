7c2dcc539406e2fedd4312b0cd110ad1eefe8488
futbollaboratuvari <cemkaplanoglu@gmail.com>
2026-09-18 22:18:07 +0300

    Set live analysis robot cache version

--- cache-version.js
+++ cache-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "20260918-live-power-stable-v2";
+  const version = "20260918-live-analysis-robot-v1";
   const resetKey = "fl_membership_full_reset_20260622_v9";
 
   if (localStorage.getItem(resetKey) !== "1") {
